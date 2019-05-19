ce05bd6134785a2f6bcab807d0c2212c146266d0
Carlos Beltran <imthatcarlos@gmail.com>
2019-05-19 10:07:42 -0400

    Trying to fix invest script

--- scripts/invest.js
+++ scripts/invest.js
@@ -1,7 +1,10 @@
 require('dotenv').config();
 
+const contract = require('truffle-contract');
 const Main = require('./../build/contracts/Main.json');
 const StableToken = require('./../build/contracts/StableToken.json');
+const AssetToken = require('./../build/contracts/AssetToken.json');
+const getContracts = require('./../src/utils/getContracts');
 
 // let's say every token will be worth 10 cents when asset is first fractionalized
 const VALUE_PER_TOKEN_USD_CENTS = 10;
@@ -15,7 +18,10 @@ module.exports = async function(callback) {
 
     // get first asset to invest in
     assetData = await contracts.assetRegistry.getAssetById(1);
-    assetToken = await AssetToken.at(assetData.tokenAddress);
+    console.log(assetData);
+    assetTokenC = contract({ ...AssetToken, address: assetData.tokenAddress });
+    assetTokenC.setProvider(contracts.web3.currentProvider);
+    let assetToken = await assetTokenC.deployed();
 
     const cap = contracts.web3.utils.fromWei(await assetToken.cap());
     // invest 10%
